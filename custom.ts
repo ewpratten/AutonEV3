be014f5c3899c6e08a8d312f8641d412486f123b
Evan Pratten <ewpratten@gmail.com>
2020-03-10 10:56:21 -0400

    Added localization
    Co-authored-by: William Meathrel <wcmeathrel@gmail.com>
    Co-authored-by: b3alzach1n <monsterkid@icloud.com>

--- custom.ts
+++ custom.ts
@@ -1,9 +1,253 @@
+/* ########## Main  ########## */
+
+// Path definition
+class Path {
+    private scale: number;
+
+    public waypoints: Pose[];
+
+    public Path() {
+
+        // Create list of waypoints
+
+
+    }
+}
+
+class Constants {
+    // TODO add real values
+    public left_motor_port = "a";
+    public right_motor_port = "b";
+    public gyro = 1;
+
+}
+
+class config {
+    public RampRate = 1;
+}
+
+
+/**
+ * All main code shall be run from here to reduce issues with cross-compiling to "blocks mode"
+ */
+function loop() {
+
+}
+
+/* ########## MathUtils ########## */
+
+/**
+ * Convert degrees to radians
+ * @param degrees Degrees
+ */
+function d2r(degrees: number) {
+    var pi = Math.PI;
+    return degrees * (pi / 180);
+}
+
+/**
+ * Convert radians to degrees
+ * @param radians Radians
+ */
+function r2d(radians: number) {
+    var pi = Math.PI;
+    return radians * (180 / pi);
+}
+
+/**
+ * Returns value clamped between low and high boundaries.
+ *
+ * @param value Value to clamp.
+ * @param min   The lower boundary to which to clamp value.
+ * @param max  The higher boundary to which to clamp value.
+ */
+function clamp(val: number, min: number, max: number) {
+    return Math.max(min, Math.min(val, max));
+}
+
+/* ########## Gyro Class ########## */
+
+class Gyro {
+
+    public gyro_port: string;
+
+    constructor() {
+        
+    }
+
+    public getDegrees() {
+        return sensors.gyro1.getAngle();
+    }
+
+
+
+}
+
+
+/* ########## Kinematics ########## */
+
+class DriveSignal {
+    public left_goal: number = 0.0;
+    public right_goal: number = 0.0;
+
+    /**
+     * 
+     * @param left left goal value
+     * @param right right goal value
+     */
+    public DriveSignal(left: number, right: number) {
+        this.left_goal = left;
+        this.right_goal = right;
+    }
+}
+
+
+
 /* ########## Localization ########## */
 
+class Rotation {
+    private rads: number = 0.0;
 
+    /**
+     * 
+     * @param rads Angle in radians
+     */
+    constructor(rads: number) {
+        this.rads = rads;
+    }
+
+    public getDegrees() {
+        return r2d(this.rads);
+    }
+
+    public getRadians() {
+        return this.rads;
+    }
+
+    public setRadians(rads: number) {
+        this.rads = rads;
+    }
+
+    public fromPoint(x: number, y: number): Rotation {
+        var mag: number = Math.hypot(x, y);
+        var sin, cos;
+        if (mag > 1e-6) {
+            sin = y / mag;
+            cos = x / mag;
+        } else {
+            sin = 0;
+            cos = 1;
+        }
+
+        return new Rotation(Math.atan2(sin, cos));
+
+    }
+
+    public rotateBy(other: Rotation): Rotation {
+        var x: number = Math.cos(this.rads) * Math.cos(other.getRadians()) - Math.sin(this.rads) * Math.sin(other.getRadians());
+        var y: number = Math.cos(this.rads) * Math.sin(other.getRadians()) - Math.sin(this.rads) * Math.cos(other.getRadians());
+
+        return this.fromPoint(x, y);
+    }
+
+    public unaryMinus(): Rotation {
+        return new Rotation(this.rads * -1);
+    }
+
+    public minus(other: Rotation): Rotation {
+        return this.rotateBy(other.unaryMinus());
+    }
+}
+
+/**
+ * Create a rotation using degrees
+ * @param degrees Degree angle
+ */
+function createRotationDegrees(degrees: number): Rotation {
+    return new Rotation(r2d(degrees));
+}
+
+/**
+ * A position in 2D space
+ */
 class Pose {
     public x: number;
     public y: number;
-    public theta: number;
+    public theta: Rotation;
+
+    /**
+     * Create a pose in 2D space
+     * @param x X component
+     * @param y Y component
+     * @param theta Angle
+     */
+    constructor(x: number, y: number, theta: Rotation = new Rotation(0.0)) {
+        this.x = x;
+        this.y = y;
+        this.theta = theta;
+    }
 }
 
+
+/**
+ * Create a pose that follows a scaling factor
+ * @param x  X component
+ * @param y  Y component
+ * @param theta  Angle in degrees
+ * @param scale the scaling multiplier
+ * @return returns a scaled posed
+ */
+function createScaledPose(x: number, y: number, theta: number, scale: number): Pose {
+    return new Pose(x * scale, y * scale, createRotationDegrees(theta));
+}
+
+/**
+ * A tool for localizing a differential drivebase
+ */
+class Localizer {
+
+    private gyroOffset: Rotation;
+    private prevAngle: Rotation;
+
+    private pose: Pose;
+    private prevDistLeft: number;
+    private prevDistRight: number;
+
+    constructor(startAngle: Rotation, startPose: Pose) {
+        this.pose = startPose;
+        this.gyroOffset = startAngle.minus(startPose.theta);
+        this.prevAngle = startPose.theta;
+
+        this.prevDistLeft = 0.0;
+        this.prevDistRight = 0.0;
+    }
+
+    public reset(pose: Pose, angle: Rotation) {
+        this.pose = pose;
+        this.prevAngle = pose.theta;
+        this.gyroOffset = pose.theta.minus(angle);
+
+        this.prevDistLeft = 0.0;
+        this.prevDistRight = 0.0;
+    }
+
+    public update(currentAngle: Rotation, leftMeters: number, rightMeters: number) {
+        var deltaLeft: number = leftMeters - this.prevDistLeft;
+        var deltaRight: number = rightMeters - this.prevDistRight;
+
+        this.prevDistLeft = leftMeters;
+        this.prevDistRight = rightMeters;
+
+        var deltaPose: number = (deltaLeft + deltaRight) / 2.0;
+        var angle: Rotation = currentAngle.rotateBy(this.gyroOffset);
+
+        // TODO: These may need to be flipped cos/sin
+        var newX: number = this.pose.x + (deltaPose * Math.sin(angle.getRadians()));
+        var newY: number = this.pose.y + (deltaPose * Math.cos(angle.getRadians()));
+
+        this.pose.x = newX;
+        this.pose.y = newY;
+        this.pose.theta = angle;
+
+    }
+}
